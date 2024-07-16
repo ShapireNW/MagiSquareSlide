ae05f9ad5bbdd96c88b5bc7a19cdd13dba0bf818
ShapireNW <aaronjyri@gmail.com>
2024-07-16 16:34:43 +0300

    Add files via upload

---
+++ SlidingPuzzle/README.md
@@ -0,0 +1,11 @@
+# 004_Improved_Sliding_Puzzle_Godot
+A simple sliding puzzle game made with Godot Engine 4.
+
+An image of 1024 by 1024 pixels is split into the 16 tiles. The tiles are randomly shuffled at the start of the game and you should order the tiles to complete the image.
+
+You can watch the video on how I made it here:
+
+https://youtu.be/zevIsYDnOJo
+
+
+
---
+++ SlidingPuzzle/src/main.gd
@@ -0,0 +1,142 @@
+extends Area2D
+@onready var collision_shape_2d = $CollisionShape2D
+@onready var full_image = $FullImage
+
+var tiles = []
+var solved = []
+var mouse = false
+var tile = preload("res://tile.tscn")
+var tile_h
+var offset
+var t
+var movecounter = 0
+var previous = ""
+var areaposx
+var areaposy
+var tilesize
+
+func _ready():
+	start_game()
+	
+
+func start_game():
+	areaposx = get_window().get_size().x
+	collision_shape_2d.position.x=areaposx/2
+	areaposy = get_window().get_size().y
+	collision_shape_2d.position.y=areaposy/2
+	if areaposy > areaposx:
+		collision_shape_2d.shape.size.x=areaposx
+		collision_shape_2d.shape.size.y=areaposx
+	else:
+		collision_shape_2d.shape.size.x=areaposy
+		collision_shape_2d.shape.size.y=areaposy
+	tile_h = collision_shape_2d.shape.size.x/4
+	var image = Image.load_from_file("res://img/Thousand yard lorax.png")
+	var texture = ImageTexture.create_from_image(image)
+	var greyimage = Image.load_from_file("res://img/greytile.png")
+	var greytexture = ImageTexture.create_from_image(greyimage)
+	$FullImage.texture = texture
+	$FullImage.position.x = areaposx
+	$FullImage.position.y = areaposy/2
+	
+	for j in range(0,4):
+		for i in range(0,4):
+			var myregion = Rect2i(i * tile_h,j * tile_h,tile_h,tile_h)
+			var newimage = image.get_region(myregion)
+			var newtexture = ImageTexture.create_from_image(newimage)
+			var newtile = tile.instantiate()
+			newtile.position.x = tile_h * i + tile_h/2 + i*2
+			newtile.position.y = tile_h * j + tile_h/2 + j*2
+			newtile.tilename = "Tile" + str(j * 4 + i + 1)
+			if newtile.tilename == "Tile16":
+				newtile.tiletexture = greytexture
+				newtile.realtexture = newtexture
+			else:
+				newtile.tiletexture = newtexture
+			print(newtile.tilename)
+			add_child(newtile)
+			tiles.append(newtile)
+
+	solved = tiles.duplicate()
+	shuffle_tiles()
+	movecounter = 0
+
+func shuffle_tiles():
+	offset = tile_h + 2
+	t = 0
+	while t < 30:
+		var atile = randi() % 16
+		if tiles[atile].tilename != "Tile16" and tiles[atile].tilename != previous:
+			var rows = int(tiles[atile].position.y / offset)
+			var cols = int(tiles[atile].position.x / offset)
+			check_neighbours(rows,cols)
+
+
+
+func _process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and mouse:
+		var mouse_copy = mouse
+		print(mouse.position)
+		mouse = false
+		var rows = int(mouse_copy.position.y / offset)
+		var cols = int(mouse_copy.position.x / offset)
+		check_neighbours(rows,cols)
+		if tiles == solved and movecounter > 1:
+			print("You win in ")
+			$FullImage.show()
+			
+
+func check_neighbours(rows, cols):
+	var empty = false
+	var done = false
+	var pos = rows * 4 + cols
+	while !empty and !done:
+		var new_pos = tiles[pos].position
+		if rows < 3:
+			new_pos.y += offset
+			empty = find_empty(new_pos,pos)
+			new_pos.y -= offset
+		if rows > 0:
+			new_pos.y -= offset
+			empty = find_empty(new_pos,pos)
+			new_pos.y += offset
+		if cols < 3:
+			new_pos.x += offset
+			empty = find_empty(new_pos,pos)
+			new_pos.x -= offset
+		if cols > 0:
+			new_pos.x -= offset
+			empty = find_empty(new_pos,pos)
+			new_pos.x += offset
+		done = true
+
+func find_empty(position,pos):
+	var new_rows = int(position.y / offset)
+	var new_cols = int(position.x / offset)
+	var new_pos = new_rows * 4 + new_cols
+	if tiles[new_pos].tilename == "Tile16" and tiles[new_pos].tilename != previous:
+		swap_tiles(pos, new_pos)
+		t += 1
+		return true
+	else:
+		return false
+
+func swap_tiles(tile_src, tile_dst):
+	var temp_pos = tiles[tile_src].position
+	tiles[tile_src].position = tiles[tile_dst].position
+	tiles[tile_dst].position = temp_pos
+	
+	var temp_tile = tiles[tile_src]
+	tiles[tile_src] = tiles[tile_dst]
+	tiles[tile_dst] = temp_tile
+	
+	movecounter += 1
+	previous = tiles[tile_dst].tilename
+
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		mouse = event
+
+
+
